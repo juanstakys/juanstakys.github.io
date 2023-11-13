86b821e3fbf5814dcd7b0a8270a16c569aa45b4b
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-11-13 11:57:36 -0300

    Toggable info

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css'
 import "@fontsource/source-code-pro";
-import { Routes, Route, Link } from 'react-router-dom'
 import portrait from './assets/portrait.jpeg'
+import { useState } from 'react'
 
+function App() {
+  const [displayDetails, setDisplayDetails] = useState({ about: false, projects: false, contact: false })
 
-const Home = () => {
   return (
-    <>
+    <div className="App">
       {/* Header */}
-      <div className='mt-32 flex flex-row items-center justify-center gap-10'>
+      <div className='mt-32 flex flex-col items-center'>
         <img className="h-40 w-36" src={portrait} alt="My face! haha" />
         <div className='flex flex-col items-center text-transparent text-7xl bg-clip-text bg-gradient-to-r from-black to-blue-400'>
           <h1 className='text-4xl md:text-7xl'>Juan Cruz Stakys</h1>
@@ -17,61 +18,45 @@ const Home = () => {
       </div>
 
       {/* Items */}
-      <div className='flex flex-row justify-evenly xl:justify-center xl:gap-24 mt-28'>
+      <div className='flex flex-col w-full sm:w-auto xl:gap-24 mt-28 items-center'>
+
         {/* About */}
-        <div className='border-2 px-16 py-12 rounded-xl bg-gradient-to-br from-white to-neutral-100 hover:bg-gradient-to-tl'>
-          <Link className='text-xl xl:text-4xl' to='/about'>{"<About/>"}</Link>
+        <div className='w-1/2 mb-4 border-2 py-12 rounded-xl bg-gradient-to-br from-white to-neutral-100 hover:bg-gradient-to-tl' onClick={() => setDisplayDetails({ about: !displayDetails.about, projects: false, contact: false })}>
+          <h3 className='text-xl xl:text-4xl text-center'>{"<About/>"}</h3>
         </div>
+        {displayDetails.about &&
+          <div className='w-3/4 mb-4 border-2 px-4 py-3 rounded-xl bg-gradient-to-tl from-white to-blue-400'>
+            <p className='text-md'>
+              I'm a fullstack software developer, Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam quae tenetur atque aspernatur eos. Quis blanditiis facilis soluta quidem pariatur ea alias libero adipisci vel reiciendis, aperiam odit officia. Delectus.
+            </p>
+          </div>
+        }
+
         {/* Projects */}
-        <div className='border-2 px-16 py-12 rounded-xl bg-gradient-to-br from-white to-neutral-100 hover:bg-gradient-to-t'>
-          <Link className='text-xl xl:text-4xl' to='/projects'>{"<Projects/>"}</Link>
+        <div className='w-1/2 mb-4 border-2 py-12 rounded-xl bg-gradient-to-br from-white to-neutral-100 hover:bg-gradient-to-t' onClick={() => setDisplayDetails({ about: false, projects: !displayDetails.projects, contact: false })}>
+          <h3 className='text-xl xl:text-4xl text-center'>{"<Projects/>"}</h3>
         </div>
+        {displayDetails.projects &&
+          <div className='w-3/4 mb-4 border-2 px-4 py-3 rounded-xl bg-gradient-to-br from-white to-blue-400'>
+            <p className='text-md'>
+              I'm a fullstack software developer, Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam quae tenetur atque aspernatur eos. Quis blanditiis facilis soluta quidem pariatur ea alias libero adipisci vel reiciendis, aperiam odit officia. Delectus.
+            </p>
+          </div>
+        }
+
         {/* Contact */}
-        <div className='border-2 px-16 py-12 rounded-xl bg-gradient-to-br from-white to-neutral-100 hover:bg-gradient-to-r'>
-          <Link className='text-xl xl:text-4xl' to='/contact'>{"<Contact/>"}</Link>
+        <div className='w-1/2 mb-4 border-2 py-12 rounded-xl bg-gradient-to-br from-white to-neutral-100 hover:bg-gradient-to-r' onClick={() => setDisplayDetails({ about: false, projects: false, contact: !displayDetails.contact })}>
+          <h3 className='text-xl xl:text-4xl text-center'>{"<Contact/>"}</h3>
         </div>
+        {displayDetails.contact &&
+          <div className='w-3/4 mb-4 border-2 px-4 py-3 rounded-xl bg-gradient-to-b from-white to-green-400'>
+            <p className='text-md'>
+              I'm a fullstack software developer, Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam quae tenetur atque aspernatur eos. Quis blanditiis facilis soluta quidem pariatur ea alias libero adipisci vel reiciendis, aperiam odit officia. Delectus.
+            </p>
+          </div>
+        }
+
       </div>
-    </>
-  )
-}
-
-const About = () => {
-  return (
-    <div>
-      <h1>About me</h1>
-      <p>My name is Juan Cruz Stakys, I'm a fullstack developer from Argentina.</p>
-    </div>
-  )
-}
-
-const Projects = () => {
-  return (
-    <div>
-      <h1>Projects</h1>
-      <p>Here are some of my projects.</p>
-    </div>
-  )
-}
-
-const Contact = () => {
-  return (
-    <div>
-      <h1>Contact</h1>
-      <p>You can contact me at <a href="mailto: stakys.jc@gmail.com">stakys.jc@gmail.com</a>.</p>
-    </div>
-  )
-}
-
-function App() {
-
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
     </div>
   )
 }
